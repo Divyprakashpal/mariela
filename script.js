d6a554e37ccc269bbb8e875d8bf17e23537774a2
divyprakash123 <divyprakashpal123@gmail.com>
2023-12-07 16:45:18 +0530

    first commihhht

---
+++ script.js
@@ -0,0 +1,19 @@
+var full = document.querySelector(".navbar")
+var menubar = document.querySelector(".fullscreen")
+ var flage = 0
+full.addEventListener("click",function(){
+
+if(flage==0){
+    menubar.style.top = "100"
+    flage = 1
+
+
+
+    
+
+}
+else{
+
+}
+    
+})
